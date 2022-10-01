22677af413b8c61761274bddf211012416e9cee3
Haiden Brown <haidenbrown06@icloud.com>
2022-10-01 16:42:11 -0500

    Wrote more classes and methods

--- Student.java
+++ Student.java
@@ -1,22 +1,23 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Student implements Serializable {
 
-    private String name, courses, bestClass;
+    private String name,  bestClass;
+    private ArrayList<Course> classes;
     private int gradeLevel;
     private double GPA;
 
-    public Student(String name, int gradeLevel, String courses){
+    public Student(String name, int gradeLevel, ArrayList<Course> classes){
 
         this.name = name;
         this.gradeLevel = gradeLevel;
-        this.courses = courses;
+        this.classes = classes;
         GPA = 0.0;
         bestClass = null;
 
-
     }
-
+    //GETTERS AND SETTERS
     public String getName(){
 
         return name;
@@ -25,6 +26,16 @@ public class Student implements Serializable {
     public int getGradeLevel(){
 
         return gradeLevel;
+
+    }
+    public void setName(String name){
+
+        this.name = name;
+
+    }
+    public void setGradeLevel(int level){
+
+        this.gradeLevel = level;
 
     }
     public double calculateGPA(){
@@ -37,14 +48,14 @@ public class Student implements Serializable {
         return null;
 
     }
-    public void addCourse(int index){
+    public void addCourse(Course c){
 
-
+        classes.add(c);
 
     }
     public void removeCourse(int index){
 
-
+        classes.remove(index);
 
     }
     public String toString(){
--- TEAMS.java
+++ TEAMS.java
@@ -39,26 +39,81 @@ public class TEAMS{
         int ans = 0;
 
         while(ans != -1){
-            System.out.println("####WELCOME TO TEAMS####");
-            System.out.println("1. Add Student");
-            System.out.println("2. Add Teacher");
-            System.out.println("3. Add Course");
-            System.out.println("4. Edit Student");
-            System.out.println("5. List Students");
-            System.out.println("6. List Courses");
-            System.out.println("7. Search For Students");
-            System.out.println("-1. Exit");
-
+            
+            TEAMS.menuOne();
             ans = sc.nextInt();
 
             if(ans == 1){
 
+                int gradeLevel, numCourses;
+                String name;
+                ArrayList<Course> classes = null;
+
+                System.out.println("Student Name: ");
+                name = sc.nextLine();
+
+                System.out.println("Student Grade Level: ");
+                gradeLevel = sc.nextInt();
+
+                System.out.println("Number of Courses: ");
+                numCourses = sc.nextInt();
+
+                for(int i = 0; i < numCourses; i++){
+
+
+                    System.out.println("Course Name: ");
+                    String courseName = sc.nextLine();
+
+                    System.out.println("Teacher: ");
+                    String teacherName = sc.nextLine();
+
+                    classes.add(new Course(courseName, teacherName));
+
+                }
+
+                addStudent(new Student(name, gradeLevel, classes));
+
+            }
+            else if(ans == 2){
+
+                int yearsOfExperience;
+                String name;
+                ArrayList<Course> classes = null;
+
 
 
             }
-        }
-        
+            else if(ans == 3){
 
+
+                
+            }
+            else if(ans == 4){
+
+
+                
+            }
+            else if(ans == 5){
+
+
+                
+            }
+            else if(ans == 6){
+
+
+                
+            }
+            else if(ans == 7){
+
+
+                
+            }
+            else if(ans == -1){
+
+                System.exit(0);
+                
+            }
+        } 
     }
 
     //#### DO NOT CHANGE THIS FUNCTION ####
@@ -92,6 +147,21 @@ public class TEAMS{
         return o;
     }
 
+    //MENUS
+    public static void menuOne(){
+
+        System.out.println("####WELCOME TO TEAMS####");
+        System.out.println("1) Add Student");
+        System.out.println("2) Add Teacher");
+        System.out.println("3) Add Course");
+        System.out.println("4) Edit Student");
+        System.out.println("5) List Students");
+        System.out.println("6) List Courses");
+        System.out.println("7) Search For Students");
+        System.out.println("-1) Exit");
+
+    }
+
     //GETTERS && SETTERS
     public static void addStudent(Student s){
 
@@ -108,9 +178,28 @@ public class TEAMS{
         courses.add(c);
 
     }
-    public static void editStudent(Student s){
+    public static void editStudent(){
 
+        listStudents();
+        System.out.println("Student Index: ");
+        int index = sc.nextInt();
 
+        System.out.println("What do you want to change?");
+        System.out.println("1) Name");
+        System.out.println("2) Grade Level");
+        System.out.println("3) Add Course");
+        System.out.println("4) Remove Course");
+
+        (students.get(index)).setName("John");
+
+    }
+    public static void listStudents(){
+
+        for (int i = 0; i < students.size();i++){
+
+            System.out.print(i + ") " + students.get(i) + ", " );
+
+        }
 
     }
 
